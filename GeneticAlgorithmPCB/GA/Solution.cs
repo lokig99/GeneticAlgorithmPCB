36cfb62f095d7e32dcafac53adf8b77e141283e2
Mateusz Groblicki <lokigameplayer@gmail.com>
2021-03-08 10:15:25 +0100

    Add project files.

---
+++ GeneticAlgorithmPCB/GA/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace GeneticAlgorithmPCB.GA
+{
+    readonly struct Point
+    {
+        public readonly int X, Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    class Solution
+    {
+    }
+}
